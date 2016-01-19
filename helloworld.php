ae64d57fd444b19a18ee629461bf782d1b6e93f0
Chirag Bhatia <chiragbhatia94@gmail.com>
2016-01-19 20:12:39 +0530

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chira
+ * Date: 19-01-2016
+ * Time: 20:07
+ */
+echo "Hello, World!";
+?>
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chira
+ * Date: 19-01-2016
+ * Time: 20:09
+ */
+phpinfo();
+?>
