31e0dfdfbfb778cd325bfb8e027756ef66ee22a3
Chirag Bhatia <chiragbhatia94@gmail.com>
2016-01-25 15:33:49 +0530

    25.01.2016

---
+++ objectassignment.php
@@ -0,0 +1,32 @@
+
+<?php
+class SimpleClass
+{
+    // property declaration
+    public $var = 'a default value';
+
+    // method declaration
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+
+$instance = new SimpleClass();
+
+$assigned   =  $instance;
+$reference  =& $instance;
+
+$instance->var = '$assigned will have this value';
+
+$instance = null; // $instance and $reference become null
+
+var_dump($instance);
+var_dump($reference);
+var_dump($assigned);
+
+//produced error
+@$instance->var = '$assigned will have this value';
+
+$assigned->var = '$assigned will have this vae';
+var_dump(@$assigned);
+?>
